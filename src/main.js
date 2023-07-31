6bf020cd2143e3129e12166ecf1c7bd61295b923
Kevin Cubillos <kevinlexr@gmail.com>
2023-07-31 15:24:17 -0500

    chore: add config for axios, bootstrap and normalize

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'normalize.css'
 
-createApp(App).mount('#app')
+/* Define base url for all requests */
+axios.defaults.baseURL = 'http://localhost:8080/api/v1/';
+
+const app = createApp(App);
+app.mount('#app')
+
+import 'bootstrap/dist/js/bootstrap.js'
