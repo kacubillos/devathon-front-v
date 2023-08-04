e51eb7a390df3c2330e53a20b3e78c4980351334
Kevin Cubillos <kevinlexr@gmail.com>
2023-08-03 22:33:44 -0500

    feat: create reset password form

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginView from "@/views/LoginView.vue";
 import ForgotPwdView from "@/views/ForgotPwdView.vue";
+import ResetPwdView from "@/views/ResetPwdView.vue";
 
 const routes = [
   {
     path: "/login",
+    name: "login",
     component: LoginView,
   },
   {
     path: "/login/forgot",
+    name: "forgotPwd",
     component: ForgotPwdView,
+  },
+  {
+    path: "/login/reset",
+    name: "resetPwd",
+    component: ResetPwdView,
   },
 ];
 
---
+++ src/views/ResetPwdView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="row align-items-start align-items-md-center justify-content-center"
+  >
+    <section
+      class="text-center col-12 col-md-10 col-lg-6 px-5 py-4 my-4 mb-4 card w-100"
+    >
+      <h1>Crea una nueva contraseña</h1>
+      <form class="my-3" @submit.prevent="recoveryPassword">
+        <input
+          id="inputPassword"
+          type="password"
+          class="form-control mb-4"
+          placeholder="Nueva contraseña"
+          required
+        />
+        <input
+          id="inputRepeatPassword"
+          type="password"
+          class="form-control mb-4"
+          placeholder="Repetir nueva contraseña"
+          required
+        />
+        <button type="submit" class="btn btn-primary w-100">
+          Crear contraseña
+        </button>
+      </form>
+    </section>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const recoveryPassword = () => {
+  router.push({ name: "login" });
+};
+</script>
