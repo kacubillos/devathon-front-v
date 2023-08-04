2fe02960f7907822770d640ad890e226c378de7e
Kevin Cubillos <kevinlexr@gmail.com>
2023-08-03 21:51:57 -0500

    feat: create forgot password form

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginView from "@/views/LoginView.vue";
+import ForgotPwdView from "@/views/ForgotPwdView.vue";
 
 const routes = [
   {
     path: "/login",
     component: LoginView,
+  },
+  {
+    path: "/login/forgot",
+    component: ForgotPwdView,
   },
 ];
 
